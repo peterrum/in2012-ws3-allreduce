01960342169a293fd074ce01d02757691dc518e4
peterrum <peterrmuench@aol.com>
2016-11-14 19:39:12 +0100

    Implement MPI_Reduce for rank 0

--- src/main.cpp
+++ src/main.cpp
@@ -25,7 +25,7 @@ int MPI_Allreduce_My(T *sendbuf, T *recvbuf, int count,
 
   // go through all communication levels
   for (int i = 0; i < levels; i++) {
-    // increase size fo block
+    // increase size of block
     distance *= 2;
     // determine destination rank
     to = (rank + distance / 2) % distance + (rank / distance) * distance;
@@ -51,6 +51,62 @@ int MPI_Allreduce_My(T *sendbuf, T *recvbuf, int count,
   *sendbuf = temp;
 }
 
+template <class T>
+int MPI_Reduce_My(T *sendbuf, T *recvbuf, int count,
+                  MPI_Datatype datatype, MPI_Op op, MPI_Comm comm) {
+
+  // save local value
+  T temp1 = *sendbuf;
+  T temp2 = *recvbuf;
+
+  // my rank, number of processors, destination rank
+  int rank, nproc, to;
+  // size of subblock
+  int distance = 1;
+
+  // determin my rank and number of processors
+  MPI_Comm_size(MPI_COMM_WORLD, &nproc);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  // number of communication levels log(n) with base 2
+  int levels = log(nproc) / log(2);
+
+  // go through all communication levels
+  for (int i = 0; i < levels; i++) {
+    // increase size of block
+    distance *= 2;
+    if ((rank / distance) * distance == rank) {
+      // determine if rank has to recv
+      MPI_Recv(recvbuf, count, datatype,
+               rank + distance / 2, 0, comm, MPI_STATUSES_IGNORE);
+
+      // perform operations
+      if (op == MPI_SUM) {
+        *sendbuf += *recvbuf;
+      } else if (op == MPI_MIN) {
+        *sendbuf = min(*sendbuf, *recvbuf);
+      } else if (op == MPI_PROD) {
+        *sendbuf *= *recvbuf;
+      }
+      // determine if rank has to send
+    } else if ((rank / distance) * distance + (distance / 2) == rank) {
+      MPI_Send(sendbuf, count, datatype,
+               (rank / distance) * distance, 0, comm);
+    }
+
+  }
+
+  // put final result into
+  if (rank) {
+    *recvbuf = temp2;
+  } else {
+    *recvbuf = *sendbuf;
+  }
+
+  // reconstruct local value
+  *sendbuf = temp1;
+}
+
 int main(int argc, char** argv) {
 
   // initialisation stuff
@@ -64,12 +120,18 @@ int main(int argc, char** argv) {
   int global1, global2, global3;
 
   // perform reduction
-  MPI_Allreduce_My<int>(&local, &global1, 1,
-                        MPI_INTEGER, MPI_SUM, MPI_COMM_WORLD);
-  MPI_Allreduce_My<int>(&local, &global2, 1,
-                        MPI_INTEGER, MPI_MIN, MPI_COMM_WORLD);
-  MPI_Allreduce_My<int>(&local, &global3, 1,
-                        MPI_INTEGER, MPI_PROD, MPI_COMM_WORLD);
+  MPI_Reduce_My<int>(&local, &global1, 1,
+                     MPI_INTEGER, MPI_SUM, MPI_COMM_WORLD);
+  MPI_Reduce_My<int>(&local, &global2, 1,
+                     MPI_INTEGER, MPI_MIN, MPI_COMM_WORLD);
+  MPI_Reduce_My<int>(&local, &global3, 1,
+                     MPI_INTEGER, MPI_PROD, MPI_COMM_WORLD);
+  //  MPI_Allreduce_My<int>(&local, &global1, 1,
+  //                        MPI_INTEGER, MPI_SUM, MPI_COMM_WORLD);
+  //  MPI_Allreduce_My<int>(&local, &global2, 1,
+  //                        MPI_INTEGER, MPI_MIN, MPI_COMM_WORLD);
+  //  MPI_Allreduce_My<int>(&local, &global3, 1,
+  //                        MPI_INTEGER, MPI_PROD, MPI_COMM_WORLD);
 
   // output results
   if (rank == 0) {
